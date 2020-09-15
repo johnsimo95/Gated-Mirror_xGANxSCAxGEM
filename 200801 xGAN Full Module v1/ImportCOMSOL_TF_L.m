3843b9f04108a102d3d602aba35d40c9be69b7f4
John Simonaitis <johnsimo@mit.edu>
2020-09-15 11:01:11 -0400

    locking in final working code before minor tweaks to see

---
+++ 200801 xGAN Full Module v1/ImportCOMSOL_TF_L.m
@@ -0,0 +1,26 @@
+function [trFnRaw, trFnSymmetric] = ImportCOMSOL_TF_L(filename, portCurrent, makeSymmetric)
+%IMPORTCOMSOL_TF Summary of this function goes here
+%   Detailed explanation goes here
+
+    data=readtable(filename,'HeaderLines',5);
+    data=table2array(data);
+
+    % NO DC TERM IN RAW!
+    trFnRaw.freq = (data(1:500,1)).';
+    trFnRaw.fieldTF = (data(1:500,2)./portCurrent).';
+    %Default state of mirror!
+    
+    if (makeSymmetric)
+        comsolFreqStep=trFnRaw.freq(1,2)-trFnRaw.freq(1,1);
+        posFreq=trFnRaw.freq;
+        negFreq=-1*flip(posFreq);
+        % Ordering of FFT is 0-Pos-Neg
+        trFnSymmetric.freq = [0,posFreq,negFreq]; %add DC term!
+
+        mirrorTFpos=(trFnRaw.fieldTF);%/length(posFreq);
+        mirrorTFneg=flip(conj(mirrorTFpos)); %new side conjugate symmetric!!
+        trFnSymmetric.fieldTF = [0, mirrorTFpos, mirrorTFneg]; %setting DC term to 0! AC coupling...
+    end
+
+end
+
